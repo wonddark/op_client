a6eafc954b4bff3d2b2988572a0a7524132b1004
Osmanys F. Lomba <ossmanys@gmail.com>
2026-05-13 22:50:40 +0400

    feat(search): add SearchApiClient

---
+++ composeApp/src/commonMain/kotlin/com/opclient/search/data/SearchApiClient.kt
@@ -0,0 +1,20 @@
+package com.opclient.search.data
+
+import com.opclient.core.ApiClient
+import com.opclient.core.ApiError
+import com.opclient.core.Result
+import io.ktor.client.HttpClient
+
+class SearchApiClient(httpClient: HttpClient) :
+    ApiClient(baseUrl = "https://openlibrary.org", httpClient = httpClient) {
+
+    suspend fun search(
+        query: String,
+        offset: Int,
+        limit: Int,
+    ): Result<SearchResponseDto, ApiError> =
+        get<SearchResponseDto>(
+            path = "/search.json",
+            queryParams = mapOf("q" to query, "offset" to "$offset", "limit" to "$limit"),
+        )
+}
